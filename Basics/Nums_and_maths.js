fe63bd8965b13c5f013be444868b6e5c22d1c7ae
Gautam-codess <gs784428@gmail.com>
2024-08-23 13:35:11 +0000

    Math and num

---
+++ Basics/Nums_and_maths.js
@@ -0,0 +1,23 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length)
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.899
+console.log(otherNumber.toPrecision(3));
+
+
+//  ++++++++++++++++++++ Maths ++++++++++++++++++++
+
+console.log(Math.PI);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.max(4,5,8,10));
+console.log(Math.random());
+
+
+
